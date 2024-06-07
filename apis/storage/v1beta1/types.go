c3a7d8387defcda308c629e2710d6b44d2f01834
Simon Malpel <smalpel@sigma.fr>
2024-06-07 09:34:54 +0200

    add(storage): Backend storage manifest

---
+++ apis/storage/v1beta1/types.go
@@ -0,0 +1,47 @@
+package v1beta1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +k8s:prerelease-lifecycle-gen:introduced=1.0.0
+
+type StorageBackend struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// Standard object's metadata.
+	// More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+
+	// Specification of the desired behavior of a StorageBackend.
+	// +optional
+	Spec StorageBackendSpec `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +k8s:prerelease-lifecycle-gen:introduced=1.0.0
+
+type StorageBackendList struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// Standard list metadata.
+	// More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
+	// +optional
+	metav1.ListMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+
+	// items is the list of StorageBackend.
+	Items []StorageBackend `json:"items" protobuf:"bytes,2,rep,name=items"`
+}
+
+type StorageBackendSpec struct {
+	// Certificates used to secure etcd communication.
+	// +optional
+	Endpoints []string `json:"endpoints" protobuf:"bytes,1,opt,name=endpoints"`
+
+	// Certificates used to secure etcd communication.
+	// +optional
+	Certificates string `json:"certificates" protobuf:"bytes,2,opt,name=certificates"`
+}
--- hack/protoc.sh
+++ hack/protoc.sh
@@ -5,6 +5,7 @@ source "$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)/utils
 PKGS=(
     github.com/SimonRTC/kubeception/apis/clusters/v1beta1=kubeception.io.apis.clusters.v1beta1
     github.com/SimonRTC/kubeception/apis/nodes/v1beta1=kubeception.io.apis.nodes.v1beta1
+    github.com/SimonRTC/kubeception/apis/storage/v1beta1=kubeception.io.apis.storage.v1beta1
 )
 
 APIMACHINERY_PKGS=(
