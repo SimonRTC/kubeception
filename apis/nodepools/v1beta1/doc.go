49f5ef6a11585e81f06012609e3ecb760f7862cb
Simon Malpel <smalpel@sigma.fr>
2024-06-10 09:41:01 +0200

    fix(ghosts): Various fixes on deleted folders

--- apis/nodepools/v1beta1/doc.go
+++
@@ -1,7 +0,0 @@
-// +k8s:deepcopy-gen=package
-// +k8s:protobuf-gen=package
-// +k8s:openapi-gen=true
-// +k8s:prerelease-lifecycle-gen=true
-// +groupName=nodepools.kubeception.io
-
-package v1beta1
